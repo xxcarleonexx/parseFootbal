bc9f376efd7341bfb17d8a9537a8eddc880b915c
carleone <rusanovserg@gmail.com>
2018-03-12 10:02:03 +0200

    Continue working.

--- controllers/ParseController.php
+++ controllers/ParseController.php
@@ -4,14 +4,32 @@
 namespace controllers;
 
 
+use Clue\React\Buzz\Browser;
+use React\EventLoop\Factory;
+use services\Parser;
 use yii\console\Controller;
+use yii\helpers\VarDumper;
 
 class ParseController extends Controller
 {
 
     public function actionParse()
     {
-        
+        $loop = Factory::create();
+
+        $client = new Browser($loop);
+
+        $parser = new Parser($client);
+
+        $parser->parse([
+            'http://www.imdb.com/title/tt1270797/',
+            'http://www.imdb.com/title/tt2527336/',
+        ]);
+
+        $loop->run();
+
+        VarDumper::dump($parser->getMovieData());
+
     }
 
 }
---
+++ services/Parser.php
@@ -0,0 +1,69 @@
+<?php
+
+
+namespace services;
+
+
+use Clue\React\Buzz\Browser;
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Parser
+{
+
+    /** @var Browser */
+    private $client;
+
+    /** @var array */
+    private $parsed = [];
+
+    public function __construct(Browser $client)
+    {
+        $this->client = $client;
+    }
+
+    public function parse(array $urls = []) : void
+    {
+        foreach ($urls as $url) {
+            $this->client->get($url)->then(
+                function (ResponseInterface $response) {
+                    $this->parsed[] = $this->extractFromHtml((string) $response->getBody());
+                }
+            );
+        }
+    }
+
+    public function extractFromHtml($html) : array
+    {
+        $crawler = new Crawler($html);
+
+        $title = trim($crawler->filter('h1')->text());
+
+        $genres = $crawler->filter('[itemprop="genre"] a')->extract(['_text']);
+        $description = trim($crawler->filter(['itemprop="description"'])->text());
+
+        $crawler->filter('#titleDetails .txt-block')->each(
+            function(Crawler $crawler) {
+                foreach ($crawler->children() as $node) {
+                    $node->parentNode->removeChild($node);
+                }
+            }
+        );
+
+        $releaseDate = trim($crawler->filter('#titleDetails .txt-block')->eq(3)->text());
+
+        return [
+            'title' => $title,
+            'genres' => $genres,
+            'description' => $description,
+            'release_date' => $releaseDate,
+        ];
+    }
+
+
+    public function getMovieData() : array
+    {
+        return $this->parsed;
+    }
+
+}
